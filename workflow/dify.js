b9f500fc06b4a206e24e373d2df3ee6ead705a70
liuguojun920 <40522172+liuguojun920@users.noreply.github.com>
2025-03-05 17:03:13 +0800

    Update dify.js

--- workflow/dify.js
+++ workflow/dify.js
@@ -58,16 +58,15 @@ async function run(args) {
 
       messageList.push(content);
     }
-
-    //const message = messageList.join(`\n${"-".repeat(15)}\n`);
-    /**Notify.pushMessage({
-     * title: "Dify工作流定时助手",
-     * content: message,
-     * msgtype: "text"
-   *});
-    **/
+/**
+    const message = messageList.join(`\n${"-".repeat(15)}\n`);
+    Notify.pushMessage({
+      title: "Dify工作流定时助手",
+      content: message,
+      msgtype: "text"
+   });
   }
- /**
+ **/
   run(process.argv.splice(2)).catch(error => {
     Notify.pushMessage({
       title: "",
